466c111db51d87e3d7720842fcf4d937a12bc72e
martinliu <liuzh66@gmail.com>
2018-03-06 00:31:12 +0800

    add apache

---
+++ cookbooks/apache/recipes/server.rb
@@ -0,0 +1,21 @@
+#
+# Cookbook:: apache
+# Recipe:: server
+#
+# Copyright:: 2018, The Authors, All Rights Reserved.
+
+package 'httpd' do
+  action :install
+end
+
+file '/var/www/html/index.html' do
+  content '<h1>Hello, world!</h1>'
+  owner 'root'
+  group 'root'
+  mode '0755'
+  action :create
+end
+
+service 'httpd' do
+  action [:enable, :start]
+end
