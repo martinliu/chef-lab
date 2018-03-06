87c0f1de657bd2c0be3250f5eb45406688d84ba9
martinliu <liuzh66@gmail.com>
2018-03-06 22:50:01 +0800

    add template

--- cookbooks/workstation/recipes/setup.rb
+++ cookbooks/workstation/recipes/setup.rb
@@ -13,10 +13,14 @@ end
 
 
 file '/etc/motd' do
-  content 'This Server is the property of DevOps Lab'
+  content "This server is the property of Chef
+	HOSTNAME: #{node['hostname']}
+	IPADDRESS: #{node['ipaddress']}
+	CPU: #{node['cpu']['0']['mhz']}
+	MEMORY: #{node['memory']['total']}
+  "
   owner 'root'
   group 'root'
   mode '0755'
   action :create
 end
-
