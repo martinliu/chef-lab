1ebee977b78c411d5120b4d441abc83ab3062348
martinliu <liuzh66@gmail.com>
2018-03-12 21:26:06 +0800

    update motd

--- cookbooks/workstation/metadata.rb
+++ cookbooks/workstation/metadata.rb
@@ -1,10 +1,10 @@
 name 'workstation'
-maintainer 'The Authors'
-maintainer_email 'you@example.com'
+maintainer 'Martin Liu'
+maintainer_email 'martin@devoscoach.org'
 license 'All Rights Reserved'
 description 'Installs/Configures workstation'
 long_description 'Installs/Configures workstation'
-version '0.1.0'
+version '0.1.1'
 chef_version '>= 12.1' if respond_to?(:chef_version)
 
 # The `issues_url` points to the location where issues for this cookbook are
--- cookbooks/workstation/recipes/setup.rb
+++ cookbooks/workstation/recipes/setup.rb
@@ -10,17 +10,8 @@ package 'git' do
   action :install
 end
 
-
-
-file '/etc/motd' do
-  content "This server is the property of Chef
-	HOSTNAME: #{node['hostname']}
-	IPADDRESS: #{node['ipaddress']}
-	CPU: #{node['cpu']['0']['mhz']}
-	MEMORY: #{node['memory']['total']}
-  "
-  owner 'root'
-  group 'root'
-  mode '0755'
+template '/etc/motd' do
+  source 'motd.erb'
   action :create
 end
+
