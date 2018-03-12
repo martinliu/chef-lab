133388538d0685841390c735e46f37da58acefbf
martinliu <liuzh66@gmail.com>
2018-03-12 22:49:18 +0800

    updted.

--- cookbooks/apache/recipes/server.rb
+++ cookbooks/apache/recipes/server.rb
@@ -8,16 +8,11 @@ package 'httpd' do
   action :install
 end
 
-file '/var/www/html/index.html' do
-  content "<h1>Hello, world!</h1>
-    <h2> Ipaddress: #{ node['ipaddress'] } </h2>
-    <h2> Hostname: #{ node['hostname'] } </h2>
-    "
-  owner 'root'
-  group 'root'
-  mode '0755'
+template '/var/www/html/index.html' do
+  source 'index.html.erb'
   action :create
 end
+
 
 service 'httpd' do
   action [ :enable, :start ]
--- cookbooks/workstation/recipes/default.rb
+++ cookbooks/workstation/recipes/default.rb
@@ -4,4 +4,4 @@
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
 
-include setup
+include_recipe 'workstation::setup'
